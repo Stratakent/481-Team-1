404dcacea637553ca3956ed20c362f64d35fca3b
Alex Chuoy <AlexChuoy@csu.fullerton.edu>
2019-04-14 12:11:01 -0700

    Brainstorming Ideas

--- project.py
+++ project.py
@@ -3,7 +3,6 @@ import random
 import randint
 from _random
 
-# test edit
 def main():
 #this will be the AI trained to receive and destroy the image via photoshop
     class AI_creator:
@@ -13,11 +12,23 @@ def main():
         paint=r'#path to file'
         subprocess.Popen("%s %s"%(paint,pic))
         #get photoshop or paint to add in a smudge onto the photo... randomly, flip a coin or something i dunno.
-        coinflip=(randint(0,1))
+        coinflip=(randint(0,1))     #Might want to introduce more randomness so that the AI does more interesting things
         #figure out how to do that
         if coinflip==1:
             #defile image
             #state=1(true)
+
+            #Ideas of edits
+            # Horizontal Line Top
+            # Horizontal LIne Bottom
+            # Vertical Line Top
+            # Vertical LIne Bottom
+            # Diagnol Top to Bottom
+            # Diagnol Bottom to Top
+            # Draw a Circle
+            # Random Coloring
+            # Turn everything one color
+            # Replace with anotehr picture
         else:
             #nothing
 
@@ -29,6 +40,11 @@ def main():
         #open the new photo in the new library
         #attempt to compare the new photo with the original and determine if its real or fake.
         #If it's real or fake, announce so.
+
+        # Might want to change it so it guesses the edits
+        # To do this we shoudl see see which pixels are likely to change between two pictures
+        # and Create an alogrithm where it decides which of the pixels to go to based on what difference
+        # are found?
         if state==1:
             print("Image is a poorly made fake!")
         else:
